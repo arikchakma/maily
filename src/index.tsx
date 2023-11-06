4f0b6f379f24c78d24ed2d617bca9f976c002e4f
Arik Chakma <arikchangma@gmail.com>
2023-11-07 03:36:09 +0600

    Implement List

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import { JSONContent } from '@tiptap/core';
-import { Text } from '@react-email/text';
-import { Html } from '@react-email/html';
-import { Head } from '@react-email/head';
-import { Body } from '@react-email/body';
+import {
+	Text,
+	Tailwind,
+	Html,
+	Head,
+	Body,
+	Font,
+	Container,
+	Link,
+	Heading,
+	HeadingAs,
+	Hr,
+} from '@react-email/components';
 import { render as reactEmailRender } from '@react-email/render';
-import { generateKey } from './utils';
+import { cn, generateKey } from './utils';
 
 // {
 //   "type": "doc",
@@ -154,6 +163,12 @@ interface RenderOptions {
 	plainText?: boolean;
 }
 
+interface MarkType {
+	[key: string]: any;
+	type: string;
+	attrs?: Record<string, any> | undefined;
+}
+
 class Maily {
 	private readonly content: JSONContent;
 
@@ -164,20 +179,62 @@ class Maily {
 	render(options?: RenderOptions): string {
 		const nodes = this.content?.content || [];
 		const jsxNodes = nodes?.map((node, index) => {
-			const options: NodeOptions = {
+			const nodeOptions: NodeOptions = {
 				prev: nodes[index - 1],
 				next: nodes[index + 1],
 				parent: node,
 			};
 
-			return this.renderNode(node);
+			return this.renderNode(node, nodeOptions);
 		});
 
 		const markup = (
-			<Html>
-				<Head></Head>
-				<Body>{jsxNodes}</Body>
-			</Html>
+			<Tailwind
+				config={{
+					theme: {
+						extend: {
+							colors: {
+								heading: 'rgb(17, 24, 39)',
+								text: 'rgb(55, 65, 81)',
+								horizontal: 'rgb(234, 234, 234)',
+							},
+							fontSize: {
+								paragraph: '15px',
+							},
+						},
+					},
+				}}
+			>
+				<Html>
+					<head>
+						<meta content="text/html; charset=UTF-8" httpEquiv="Content-Type" />
+						<Font
+							fontFamily="Inter"
+							fallbackFontFamily="Verdana"
+							webFont={{
+								url: 'https://rsms.me/inter/font-files/Inter-Regular.woff2?v=3.19',
+								format: 'woff2',
+							}}
+							fontWeight={400}
+							fontStyle="normal"
+						/>
+					</head>
+					<body>
+						<Container
+							style={{
+								maxWidth: '600px',
+								minWidth: '300px',
+								width: '100%',
+								marginLeft: 'auto',
+								marginRight: 'auto',
+								padding: '0.5rem',
+							}}
+						>
+							{jsxNodes}
+						</Container>
+					</body>
+				</Html>
+			</Tailwind>
 		);
 
 		return reactEmailRender(markup, options);
@@ -185,24 +242,32 @@ class Maily {
 
 	// `getMappedContent` will call corresponding node type
 	// and return text content
-	private getMappedContent(node: JSONContent): JSX.Element[] {
+	private getMappedContent(
+		node: JSONContent,
+		options?: NodeOptions
+	): JSX.Element[] {
 		return (
-			node?.content?.map((childNode) => {
-				return this.renderNode(childNode);
-			}) || []
+			(node?.content
+				?.map((childNode) => {
+					return this.renderNode(childNode, options);
+				})
+				?.filter((node) => node !== null) as JSX.Element[]) || []
 		);
 	}
 
 	// `renderNode` will call the method of the corresponding node type
-	private renderNode(node: JSONContent): JSX.Element {
+	private renderNode(
+		node: JSONContent,
+		options?: NodeOptions
+	): JSX.Element | null {
 		const type = node.type!;
 
 		if (type in this) {
 			// @ts-ignore
-			return this[node.type]?.(node);
+			return this[node.type]?.(node, options);
 		} else {
 			console.warn(`Node type "${type}" is not supported.`);
-			return <></>;
+			return null;
 		}
 	}
 
@@ -212,11 +277,11 @@ class Maily {
 		const text = node.text || '&nbsp;';
 		const marks = node.marks || [];
 
-		return marks.reduce((acc: JSX.Element, mark) => {
+		return marks.reduce((acc, mark) => {
 			const type = mark.type!;
 			if (type in this) {
 				// @ts-ignore
-				const markElement = this[type]?.(acc);
+				const markElement = this[type]?.(mark, acc);
 				return markElement;
 			} else {
 				console.warn(`Mark type "${type}" is not supported.`);
@@ -225,11 +290,26 @@ class Maily {
 		}, <>{text}</>);
 	}
 
-	paragraph(node: JSONContent, options?: Partial<NodeOptions>): JSX.Element {
+	paragraph(node: JSONContent, options?: NodeOptions): JSX.Element {
 		const { attrs } = node;
 		const alignment = attrs?.textAlign || 'left';
 
-		return <Text key={generateKey()}>{this.getMappedContent(node)}</Text>;
+		const { parent, next } = options || {};
+		const isParentListItem = parent?.type === 'listItem';
+		const isNextSpacer = next?.type === 'spacer';
+
+		return (
+			<Text
+				key={generateKey()}
+				className={cn(
+					'text-paragraph mb-5 mt-0 text-text antialiased',
+					`text-${alignment}`,
+					isParentListItem || isNextSpacer ? 'mb-0' : ''
+				)}
+			>
+				{this.getMappedContent(node)}
+			</Text>
+		);
 	}
 
 	text(node: JSONContent) {
@@ -241,23 +321,108 @@ class Maily {
 		return text;
 	}
 
-	bold(text: string): JSX.Element {
+	bold(_: MarkType, text: string): JSX.Element {
 		return <strong key={generateKey()}>{text}</strong>;
 	}
 
-	italic(text: string): JSX.Element {
+	italic(_: MarkType, text: string): JSX.Element {
 		return <em key={generateKey()}>{text}</em>;
 	}
 
-	underline(text: string): JSX.Element {
+	underline(_: MarkType, text: string): JSX.Element {
 		return <u key={generateKey()}>{text}</u>;
 	}
 
-	strike(text: string): JSX.Element {
+	strike(_: MarkType, text: string): JSX.Element {
 		return (
 			<s key={generateKey()} style={{ textDecoration: 'line-through' }}>
 				{text}
 			</s>
+		);
+	}
+
+	link(mark: MarkType, text: string): JSX.Element {
+		const { attrs } = mark;
+		const href = attrs?.href || '#';
+		const target = attrs?.target || '_blank';
+		const rel = attrs?.rel || 'noopener noreferrer nofollow';
+
+		return (
+			<Link
+				key={generateKey()}
+				href={href}
+				target={target}
+				rel={rel}
+				className="font-medium underline text-heading"
+			>
+				{text}
+			</Link>
+		);
+	}
+
+	heading(node: JSONContent, options?: NodeOptions): JSX.Element {
+		const { attrs } = node;
+		const { next } = options || {};
+
+		const level = `h${+attrs?.level || 1}`;
+		const alignment = attrs?.textAlign || 'left';
+		const isNextSpacer = next?.type === 'spacer';
+
+		return (
+			<Heading
+				key={generateKey()}
+				as="h1"
+				className={cn('text-heading mb-3', {
+					'text-4xl font-extrabold': level === 'h1',
+					'text-3xl font-bold': level === 'h2',
+					'text-2xl leading-[38px] font-semibold': level === 'h3',
+					'mb-0': isNextSpacer,
+				})}
+				style={{
+					textAlign: alignment,
+				}}
+			>
+				{this.getMappedContent(node)}
+			</Heading>
+		);
+	}
+
+	variable(node: JSONContent): JSX.Element {
+		const { attrs } = node;
+		const variable = attrs?.id || '';
+
+		return <>{`{{${variable}}}`}</>;
+	}
+
+	horizontalRule(node: JSONContent, options?: NodeOptions): JSX.Element {
+		return <Hr className="my-8" />;
+	}
+
+	orderedList(node: JSONContent, options?: NodeOptions): JSX.Element {
+		return (
+			<Container key={generateKey()}>
+				<ol className="list-decimal mt-0 pl-[26px] mb-5">
+					{this.getMappedContent(node)}
+				</ol>
+			</Container>
+		);
+	}
+
+	bulletList(node: JSONContent, options?: NodeOptions): JSX.Element {
+		return (
+			<Container key={generateKey()}>
+				<ul key={generateKey()} className="list-disc mt-0 pl-[26px] mb-5">
+					{this.getMappedContent(node)}
+				</ul>
+			</Container>
+		);
+	}
+
+	listItem(node: JSONContent, options?: NodeOptions): JSX.Element {
+		return (
+			<li key={generateKey()} className={cn('mb-2 pl-1.5 antialiased')}>
+				{this.getMappedContent(node, { ...options, parent: node })}
+			</li>
 		);
 	}
 }
@@ -266,40 +431,124 @@ const maily = new Maily({
 	type: 'doc',
 	content: [
 		{
-			type: 'paragraph',
-			attrs: {
-				textAlign: 'left',
-			},
+			type: 'bulletList',
 			content: [
 				{
-					type: 'text',
-					text: 'Are you ready to transform your email communication? Introducing Maily, the powerful email editor that enables you to craft captivating emails effortlessly.',
+					type: 'listItem',
+					attrs: {
+						color: null,
+					},
+					content: [
+						{
+							type: 'paragraph',
+							attrs: {
+								textAlign: 'left',
+							},
+							content: [
+								{
+									type: 'text',
+									text: 'Arik Chakma',
+								},
+							],
+						},
+					],
+				},
+				{
+					type: 'listItem',
+					attrs: {
+						color: null,
+					},
+					content: [
+						{
+							type: 'paragraph',
+							attrs: {
+								textAlign: 'left',
+							},
+							content: [
+								{
+									type: 'text',
+									text: 'Hello Arikko',
+								},
+							],
+						},
+					],
+				},
+				{
+					type: 'listItem',
+					attrs: {
+						color: null,
+					},
+					content: [
+						{
+							type: 'paragraph',
+							attrs: {
+								textAlign: 'left',
+							},
+							content: [
+								{
+									type: 'text',
+									text: 'How Are you?',
+								},
+							],
+						},
+					],
 				},
 			],
 		},
 		{
-			type: 'paragraph',
+			type: 'orderedList',
 			attrs: {
-				textAlign: 'left',
+				start: 1,
 			},
 			content: [
 				{
-					type: 'text',
-					text: 'Elevate your email communication with ',
-				},
-				{
-					type: 'text',
-					marks: [
-						{ type: 'strike' },
+					type: 'listItem',
+					attrs: {
+						color: null,
+					},
+					content: [
 						{
-							type: 'bold',
+							type: 'paragraph',
+							attrs: {
+								textAlign: 'left',
+							},
+							content: [
+								{
+									type: 'text',
+									text: 'Number One',
+								},
+							],
 						},
 					],
-					text: 'Maily',
 				},
 				{
-					type: 'text',
-					text: '! Click below to try it out:',
+					type: 'listItem',
+					attrs: {
+						color: null,
+					},
+					content: [
+						{
+							type: 'paragraph',
+							attrs: {
+								textAlign: 'left',
+							},
+							content: [
+								{
+									type: 'text',
+									text: 'Number ',
+								},
+								{
+									type: 'text',
+									marks: [
+										{
+											type: 'bold',
+										},
+									],
+									text: '2',
+								},
+							],
+						},
+					],
 				},
 			],
 		},
--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,10 @@
+import { clsx, type ClassValue } from 'clsx';
+
 export function generateKey() {
 	// Length of 9 is chosen because it is the maximum length of a valid CSS selector.
 	return Math.random().toString(36).substr(2, 9);
+}
+
+export function cn(...inputs: ClassValue[]) {
+	return clsx(inputs);
 }
